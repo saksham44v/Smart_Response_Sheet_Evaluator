43e2fb180cb87fef54f90d4654d9958c4c451bf2
saksham44v <128460232+saksham44v@users.noreply.github.com>
2025-06-08 14:22:07 +0530

    Update gate_answer_keys.py

--- gate_answer_keys.py
+++ gate_answer_keys.py
@@ -1,6 +1,6 @@
 import json
 
 # Step 1: Open and load the JSON file
-with open('gatee\gate_cse_answer_key_with_marks.json', 'r') as file:
+with open('gatee/gate_cse_answer_key_with_marks.json', 'r') as file:
     answer = json.load(file)
 
